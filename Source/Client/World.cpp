8e6ea10529d14fb258847510e76d739d885d50c6
Tamir Atias <tamir@teamenso.com>
2015-03-21 01:25:06 +0200

    Fix networked entity removal

--- Source/Client/World.cpp
+++ Source/Client/World.cpp
@@ -34,6 +34,13 @@ EntityID ClientWorld::findIDByEntity(CL_Entity *entity) {
 	return 0;
 }
 
+void ClientWorld::deleteRemovedEntities() {
+	for (CL_Entity *entity : mRemovedEntities) {
+		delete entity;
+	}
+	mRemovedEntities.clear();
+}
+
 void ClientWorld::deleteAllEntities() {
 	for (CL_Entity *entity : mEntities) {
 		delete entity;
@@ -63,6 +70,8 @@ void ClientWorld::readFromSnapshot(const BitStream &snapshot) {
 	uint32_t numRemovedEnts = snapshot.readU32();
 	for (uint32_t i = 0; i < numRemovedEnts; ++i) {
 		EntityID id = snapshot.readAny<EntityID>();
+		CL_Entity *entity = findEntityByID(id);
+		removeEntity(id, entity);
 	}
 }
 
@@ -73,6 +82,22 @@ void ClientWorld::addEntity(EntityID id, CL_Entity *entity) {
 	mEntityToIDMap.insert({ entity, id });
 
 	mEntities.push_back(entity);
+}
+
+void ClientWorld::removeEntity(EntityID id, CL_Entity *entity) {
+	entity->mID = 0;
+
+	mIDToEntityMap.erase(mIDToEntityMap.find(id));
+	mEntityToIDMap.erase(mEntityToIDMap.find(entity));
+
+	for (auto it = mEntities.begin(); it != mEntities.end(); ++it) {
+		if (*it == entity) {
+			mEntities.erase(it);
+			break;
+		}
+	}
+
+	mRemovedEntities.push_back(entity);
 }
 
 void ClientWorld::update(float dt) {
